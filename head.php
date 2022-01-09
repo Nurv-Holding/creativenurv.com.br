6f8782665636ff23ed9820e42e28dd475ee46984
Bruna <89098062+brunastuart@users.noreply.github.com>
2022-01-09 16:49:54 -0300

    1.0

---
+++ head.php
@@ -0,0 +1,49 @@
+<head>
+  <!-- Required meta tags -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" href="https://i.imgur.com/2xvFKdA.png" />
+  <!-- Primary Meta Tags -->
+  <title>Creative Nurv – Marketing e Tecnologia</title>
+  <meta name="title" content="Creative Nurv – Marketing e Tecnologia">
+  <meta name="description" content="Se você quer começar expandir seu negócio ou deseja investir em um novo negócio está no lugar certo. A CreativeFranchising é uma agência">
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://creativenurv.com/">
+  <meta property="og:title" content="Creative Nurv – Marketing e Tecnologia">
+  <meta property="og:description" content="Se você quer começar expandir seu negócio ou deseja investir em um novo negócio está no lugar certo. A CreativeFranchising é uma agência">
+  <meta property="og:image" content="">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://creativenurv.com/">
+  <meta property="twitter:title" content="Creative Nurv – Marketing e Tecnologia">
+  <meta property="twitter:description" content="Se você quer começar expandir seu negócio ou deseja investir em um novo negócio está no lugar certo. A CreativeFranchising é uma agência">
+  <meta property="twitter:image" content="">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="src/css/style.css">
+  <script src="https://kit.fontawesome.com/2463b1ceea.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '426824605296582');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=426824605296582&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+</head>
